26686c05c47b65c23d65f0a122e15b6d00e0de56
Tanvi-Pandey <tanvip0709@gmail.com>
2026-06-10 10:53:36 +0530

    Deposit and Withdrwal

--- Src/BankAccount.java
+++ Src/BankAccount.java
@@ -8,58 +8,100 @@ public class BankAccount {
 
     private ArrayList<String> transactions;  //Collection to store transactions
 
+    private static final String CURRENCY ="Rs. "; //always prints with rs.
+
     public BankAccount( // Constructor - Used to initialize object
             int accountNumber,
             String accountHolderName,
-            double balance
-    )
+            double balance)
 
-    {
-        this.accountNumber = //parameter
+    {   this.accountNumber = //parameter
                 accountNumber;
         this.accountHolderName =
                 accountHolderName;
         this.balance =
                 balance;
         transactions =
-                new ArrayList<>();
+                new ArrayList<>(); //without this NullPointerException
         transactions.add(
-                "Account created with Rs."
-                        + balance
-        );
+                "Account created with "
+                        + CURRENCY
+                        + balance);
     }
 
     public int getAccountNumber() {
-        return accountNumber;
-    }
+        return accountNumber;}
     public String getAccountHolderName() {
-        return accountHolderName;
-    }
+        return accountHolderName;}
     public double getBalance() {
-        return balance;
-    }
+        return balance;}
 
     public void displayAccountDetails() {
-
     System.out.println(
             "Account Number: "
-                    + accountNumber
-    );
+                    + accountNumber);
     System.out.println(
             "Account Holder: "
-                    + accountHolderName
-    );
+                    + accountHolderName);
     System.out.println(
-            "Balance: Rs. "
-                    + balance
-    );
+            "Current Balance: "
+                     + CURRENCY
+                     + balance);
 }
 
     public void showTransactions() { //shows transactions
     for (String transaction :
             transactions) {
-
-        System.out.println(transaction);
-    }
+        System.out.println(transaction);}
 }
+
+public void deposit(
+        double amount
+) {
+    if (amount > 0) { //banks don't aloow -ve deposition
+        balance += amount; //balance updation
+        transactions.add(
+                "Amount Deposited "
+                        + CURRENCY
+                        + amount);
+        System.out.println(
+                CURRENCY
+                        + amount
+                        + " deposit successfull.");
+        System.out.println(
+                "Account Holder: "
+                        + accountHolderName);
+        System.out.println(
+                "Account Number: "
+                        + accountNumber);
+        System.out.println(
+                "Deposited Amount: "
+                        + CURRENCY
+                        + amount);
+        System.out.println(
+                "Updated Balance: "
+                        + CURRENCY
+                        + balance       );}
+        else {
+        System.out.println("Invalid deposit amount.");}
+    }
+
+public void withdraw(double amount){
+    if (amount <= 0) {
+        System.out.println("Invalid withdrawal amount.");
+        return;}
+    if (amount > balance) {
+        System.out.println("\nWithdrawal Failed!");
+        System.out.println(
+                "Account Holder: "+ accountHolderName);
+        System.out.println("Insufficient balance.");
+        return;}
+    balance -= amount; //update balance
+    transactions.add(
+            "Withdrawal: - "+ CURRENCY+ amount);
+System.out.println("\nWithdrawal Successful!");
+System.out.println( "Account Holder: "+ accountHolderName);
+System.out.println("Account Number: "+ accountNumber);
+System.out.println("Withdrawn Amount: "+ CURRENCY+ amount);
+System.out.println("Remaining Balance: "+ CURRENCY+ balance);}
 }
--- Src/Main.java
+++ Src/Main.java
@@ -2,41 +2,38 @@ public class Main {
 
     public static void main(String[] args) {
 
-        BankAccount account1 = // Creating a bank account object
-                new BankAccount(
-                        101,
-                        "Tanvi",
-                        5000
-                );
-        BankAccount account2 =
-                new BankAccount(
-                        102,
-                        "Vedaant",
-                        12000
-                );
+System.out.println( //welcome line
+        "WELCOME TO SMART BANKING SYSTEM");
 
-        System.out.println(
-        "ACCOUNT DETAILS"
-);
-        System.out.println(
-        "\nAccount 1:"
-);
+BankAccount account1 = // Creating a bank account object
+        new BankAccount(
+                101,
+                "Tanvi",
+                5000);
+BankAccount account2 =
+        new BankAccount(
+                102,
+                "Vedaant",
+                12000);
+
+System.out.println("\nDEPOSIT");
+account1.deposit(2000);
+account2.deposit(5000);
+
+System.out.println("\nWITHDRAWAL");
+account1.withdraw(1000);
+account2.withdraw(15000);
+
+System.out.println("\nACCOUNT DETAILS");
+System.out.println("Account 1:");
 account1.displayAccountDetails();
+System.out.println("Account 2:");
+account2.displayAccountDetails();
 
-        System.out.println(
-        "\nAccount 2:"
-);
-        account2.displayAccountDetails();
-
-        System.out.println(
-        "\nTRANSACTION HISTORY"
-);
-        System.out.println(
-        "Account 1 (Tanvi):"
-);
-        account1.showTransactions();
-        System.out.println(
-        "Account 2 (Rahul):"
-);       account2.showTransactions();
+System.out.println("\nTRANSACTION HISTORY");
+System.out.println("Account 1 (Tanvi):");
+account1.showTransactions();
+System.out.println("Account 2 (Rahul):");       
+account2.showTransactions();
     }
 }
